c17abad18cfc8dec40ed159f5e7372e1068fe22c
apb <arminbackhaus43@gmail.com>
2026-02-24 20:24:00 +0100

    WIP symfony...

--- src/Controller/RaceController.php
+++ src/Controller/RaceController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Race;
+use App\Repository\RaceRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -14,5 +17,18 @@ final class RaceController extends AbstractController
         return $this->render('race/index.html.twig', [
             'controller_name' => 'RaceController',
         ]);
+    }
+
+    #[Route('/race/test', name: 'race_test')]
+    public function testWriteTodb(RaceRepository $rp, EntityManagerInterface $em): Response
+    {
+        $newRace = new Race();
+        $newRace->setBike("Honda");
+        $newRace->setYear("2005");
+
+        $em->persist($newRace);
+        $em->flush();
+
+        return new Response('Hello World');
     }
 }
